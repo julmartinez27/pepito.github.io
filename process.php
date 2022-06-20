30004b4114d77ec5a67750b1fa52275527858f54
julmartinez27 <90416452+julmartinez27@users.noreply.github.com>
2022-06-20 09:34:45 -0500

    Update process.php

--- process.php
+++ process.php
@@ -170,7 +170,7 @@ $json = json_decode($responseData, true);
 		</div>
 		<div class="row">
 		<div class="col-md-12 text-center">
-		<!-- <img src="../imagenes/marcas.png"> -->
+		
 		</div>
 	</div>
 	<p>Powered by <a href="https://www.datafast.com.ec/" target="_blank">Datafast</a></p>	
